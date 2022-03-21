bc91a94c78ea0b193fd8bd8df3bf16c1e678f745
Mustafa Khalil <mustafakh1997@gmail.com>
2022-03-21 23:26:07 +0200

    created a middleware folder currently containing: 1. an imageUpload middleware to handle image files with incoming requests

---
+++ middleware/file-upload.js
@@ -0,0 +1,31 @@
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
+
+const fileUpload = multer({
+  limits: 1000000, // upload size in bytes
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      console.log("destination");
+      cb(null, "uploads/images");
+    },
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      console.log("filename");
+      cb(null, uuidv4() + "." + ext);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    console.log("Filtering");
+    let error = isValid ? null : new Error("Invalid mime type!");
+    cb(error, isValid);
+  },
+});
+
+module.exports = fileUpload;
