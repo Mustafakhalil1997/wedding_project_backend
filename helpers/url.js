46769acf96b9b768fad49d1639cd9153637e7251
Mustafa Khalil <mustafakh1997@gmail.com>
2022-04-22 01:32:22 +0300

    changed to heroku url

--- helpers/url.js
+++ helpers/url.js
@@ -1,4 +1,4 @@
-// const URL = "http://8951-185-97-92-113.ngrok.io";
-const URL = "http://45a1-185-101-16-103.ngrok.io";
+// const URL = "http://45a1-185-101-16-103.ngrok.io";
+const URL = "https://wedding-project-backend.herokuapp.com/";
 
 module.exports = URL;
